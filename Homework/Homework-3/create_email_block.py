c096032e622199a211f007f293311a1dc98d35dc
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-06-12 13:12:11 +0630

    create email function after training pipeline

---
+++ Homework/Homework-3/create_email_block.py
@@ -0,0 +1,11 @@
+from prefect_email import EmailServerCredentials
+
+def create_email_block():
+    credentials = EmailServerCredentials(
+        username="example@gmail.com", ## must be your real email password
+        password="abcdefgh@!",  # must be an app password
+    )
+    credentials.save("email-block")
+
+if __name__ == "__main__":
+    create_email_block()
